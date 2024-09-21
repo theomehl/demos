e938de08a07f4edd348cea6bf6715aa16d6c1ab8
DESKTOP-08O969I\Ted <ted.mehl@gmail.com>
2024-09-21 13:03:14 -0500

    Reconfigure countdown to allow user set values

--- countdown/countdown.js
+++ countdown/countdown.js
@@ -1,13 +1,18 @@
-function countdown(endDate) {
+console.time();
+let datetime = document.querySelector('input[type="datetime-local"]');
+datetime.min = new Date(new Date().toString().split('GMT')[0]+' UTC').toISOString().split('.')[0].slice(0, -3);
+let interval; 
+
+let title = document.querySelector('h1');
+
+function countdown() {
     let days, hours, minutes, seconds;
+    let savedDate = localStorage.getItem('endDate');
+    if (!savedDate) return;
+    datetime.value = savedDate;
 
-    endDate = new Date(endDate).getTime();
-
-    if (isNaN(endDate)) {
-        return; 
-    }
-
-    setInterval(calculate, 1000);
+    let endDate = new Date(savedDate).getTime();
+    if (isNaN(endDate)) return;
 
     function calculate() {
         let startDate = new Date();
@@ -35,8 +40,30 @@ function countdown(endDate) {
             return;
         }
     }
+    calculate();
+    interval = setInterval(calculate, 1000);
 }
 
-(function () {
-    countdown('12/23/2021 5:00:00 PM');
-}());
+function getTitle() {
+    if (!localStorage.getItem('title')) {
+        title.innerHTML = 'Countdown.';
+        return;
+    }
+    title.innerHTML = localStorage.getItem('title');
+}
+
+function setTitle() {
+    localStorage.setItem('title', title.innerHTML);
+    getTitle();
+}
+
+countdown();
+
+datetime.addEventListener("change", function() {
+    localStorage.setItem('endDate', datetime.value);
+    clearInterval(interval);
+    countdown();
+});
+
+title.addEventListener("focusout", setTitle);
+getTitle();
